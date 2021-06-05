132595b2d5a740e8a0ea11d0f4b990cdf58866ab
vitoremanueldev <veslima3@gmail.com>
2021-06-05 06:14:11 -0300

    Validação

--- src/main/kotlin/br/com/zup/autores/CadastroAutorController.kt
+++ src/main/kotlin/br/com/zup/autores/CadastroAutorController.kt
@@ -1,12 +1,15 @@
 package br.com.zup.autores
 
 import io.micronaut.http.annotation.*
+import io.micronaut.validation.Validated
+import javax.validation.*
 
+@Validated
 @Controller("/autores")
 class CadastroAutorController {
 
     @Post
-    fun cadastra(@Body request: NovoAutorRequest){
+    fun cadastra(@Body @Valid request: NovoAutorRequest){
 
         println(request)
 
--- src/main/kotlin/br/com/zup/autores/NovoAutorRequest.kt
+++ src/main/kotlin/br/com/zup/autores/NovoAutorRequest.kt
@@ -1,5 +1,9 @@
 package br.com.zup.autores
 
-data class NovoAutorRequest ( val nome: String,
-                              val email: String,
-                              val descricao: String)
+import io.micronaut.core.annotation.Introspected
+import javax.validation.constraints.*
+
+@Introspected
+data class NovoAutorRequest ( @field:NotBlank val nome: String,
+                              @field:NotBlank @field:Email val email: String,
+                              @field:NotBlank @field:Size(max = 400) val descricao: String)
